d9e900a248be869d71567ae08d1c9b20fb8a223d
Mohamed Kamal <mohamed77zaki@gmail.com>
2020-07-02 12:35:24 +0200

    Draw image

--- src/com/mohamed/engine/Renderer.java
+++ src/com/mohamed/engine/Renderer.java
@@ -2,6 +2,8 @@ package com.mohamed.engine;
 
 import java.awt.image.DataBufferInt;
 
+import com.mohamed.engine.gfx.Image;
+
 public class Renderer {
 	private int pixelWidth;
 	private int pixelHeight;
@@ -15,7 +17,44 @@ public class Renderer {
 	
 	public void clear() {
 		for (int i = 0; i < pixels.length; i++) {
-			pixels[i] += 0;
+			pixels[i] = 0;
+		}
+	}
+	
+	public void setPixel(int x, int y, int value) {
+		if ((x < 0 || x >= pixelWidth || y < 0 || y >= pixelHeight) || value == 0xffff00ff) {
+			return;
+		}
+		
+		pixels[x + (y * pixelWidth)] = value;
+	}
+	
+	public void drawImage(Image image, int offX, int offY) {
+		// Don't render if all image is out of bounds
+		if (offX >= pixelWidth) return;
+		if (offY >= pixelHeight) return;
+		if (offX + image.getWidth() < 0) return;
+		if (offY + image.getHeight() < 0) return; 
+		
+		int xStart = 0;
+		int yStart = 0;
+		int renderWidth = image.getWidth();
+		int renderHeight = image.getHeight();
+		
+		// Remove image starting pixels that will not be rendered
+		if (offX < 0) {xStart += -offX;}
+		if (offY < 0) {yStart += -offY;}
+		
+		// Remove image ending pixels that will not be rendered
+		if (offX + renderWidth >= pixelWidth) {renderWidth -= offX + image.getWidth() - pixelWidth;}
+		if (offY + renderHeight >= pixelHeight) {renderHeight -= offY + image.getHeight() - pixelHeight;}
+		
+		// Render
+		for (int y = yStart; y < renderHeight; y++) {
+			for (int x = xStart; x < renderWidth; x++) {
+				int value = image.getPixels()[x + y * image.getWidth()];
+				setPixel(x + offX, y + offY, value);
+			}
 		}
 	}
 }
--- src/com/mohamed/game/GameManager.java
+++ src/com/mohamed/game/GameManager.java
@@ -5,10 +5,13 @@ import java.awt.event.KeyEvent;
 import com.mohamed.engine.AbstractGame;
 import com.mohamed.engine.GameContainer;
 import com.mohamed.engine.Renderer;
+import com.mohamed.engine.gfx.Image;
 
 public class GameManager extends AbstractGame {
+	private Image image;
+	
 	public GameManager() {
-		
+		image = new Image("/test.png");
 	}
 
 	@Override
@@ -20,7 +23,7 @@ public class GameManager extends AbstractGame {
 
 	@Override
 	public void render(GameContainer gc, Renderer renderer) {
-		
+		renderer.drawImage(image, gc.getInput().getMouseX() - 32, gc.getInput().getMouseY() - 32);
 	}
 	
 	public static void main(String args[]) {
