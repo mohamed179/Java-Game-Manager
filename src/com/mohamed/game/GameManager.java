0fd483089c1d065a0ba0b2880add0fdd0e2dbace
Mohamed Kamal <mohamed77zaki@gmail.com>
2020-07-03 16:12:41 +0200

    Sound clip

--- src/com/mohamed/game/GameManager.java
+++ src/com/mohamed/game/GameManager.java
@@ -5,23 +5,27 @@ import java.awt.event.KeyEvent;
 import com.mohamed.engine.AbstractGame;
 import com.mohamed.engine.GameContainer;
 import com.mohamed.engine.Renderer;
+import com.mohamed.engine.audio.SoundClip;
 import com.mohamed.engine.gfx.Image;
 import com.mohamed.engine.gfx.ImageTile;
 
 public class GameManager extends AbstractGame {
 	private Image image;
 	private ImageTile imageTile;
+	private SoundClip soundClip;
 	private float temp = 0;
 	
 	public GameManager() {
 		image = new Image("/test.png");
 		imageTile = new ImageTile("/test2.png", 16, 16);
+		soundClip = new SoundClip("/audios/airplanes.wav");
+		soundClip.setVolume(0);
 	}
 
 	@Override
 	public void update(GameContainer gc, float deltaTime) {
 		if (gc.getInput().isKey(KeyEvent.VK_A)) {
-			System.out.println("Key A is pressed!");
+			soundClip.play();
 		}
 		
 		temp += deltaTime * 10;
